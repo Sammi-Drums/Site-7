a7e071719c530b487f16cb3b123b6c298b35894c
Akia Samuel Chah <sammi7drums@gmail.com>
2023-02-14 01:35:45 +0100

    Update contact.js

--- Java Script/contact.js
+++ Java Script/contact.js
@@ -3,3 +3,50 @@ const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    validateoInputs();
+});
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success');
+};
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
+
+const isValidEmail = email => {
+    const re = /(([^<>()]));
+    return re.test(String(email).toLowerCase());
+}
+
+const validateoInputs = () => {
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const password = password.value.trim();
+    const password2 = password2.value.trim();
+
+    if(usernameValue === ''){
+        setError(username, 'Username is required');
+    } else {
+        setSuccess(username);
+    }
+    if(emailValue === ''){
+        setError(email, 'Email is required');
+    }
+};  
+
+
+
+
